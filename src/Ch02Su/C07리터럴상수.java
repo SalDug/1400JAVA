7978fdb2322ff7ef8cac6655d7c21412b5c0539e
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-07-11 15:42:28 +0900

    리터럴상수

---
+++ src/Ch02Su/C07리터럴상수.java
@@ -0,0 +1,30 @@
+package Ch02Su;
+
+
+public class C07리터럴상수 {
+
+	public static void main(String[] args) {
+		//상수 : 고정된 수
+		//상수 종류
+		//리터럴 상수 : 이름 부여 X 1 ,2, 3...
+		//심볼릭 상수 : 이름 부여 O (final 예약어)
+		
+		//정수 리터럴 기본자료형 : integer
+		//실수 리터럴 기본자료형 : double
+		
+		//리터럴 접미사 
+		//정수 : l, L(long)
+		//실수 : f, F(float)
+		
+//		byte num1  = (byte)129;
+//		long num2 = 123L;
+//		double num3 = 123.456;
+//		float num4 = 123.456f;
+//		System.out.println(num1);
+		
+		//int num1 = 4000000000;
+		//int num2 - 4000000000L;
+		
+	}	
+
+}
