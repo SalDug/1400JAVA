331be30eb355ae5049b755e3c8faf2feff464321
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-07-12 16:48:40 +0900

    입력

--- src/Ch04Scanner/C01Scanner.java
+++ src/Ch04Scanner/C01Scanner.java
@@ -12,11 +12,15 @@ public class C01Scanner {
 		//new Scanner(System.in)
 		//->표준 입력 스트림을 Scanner 객체에 연결
 		//->Scanner 객체는 다양한 형태의 값을 스트림으로부터 받아올 수 있는 기능을 가지는 단위
+		
+		//Scanner sc = new Scanner(System.in)
+		//->생성된 스캐너 객체의 위치값을 sc참조변수에 담아 제어함
 		Scanner sc = new Scanner(System.in);
 		
 		
 		System.out.print("첫번째 : ");
 		int num1 = sc.nextInt();
+		//sc스캐너 객체의 정수값하나를 받아 num1공간에 초기화
 		System.out.print("두번째 : ");
 		int num2 = sc.nextInt();
 		System.out.print("세번째 : ");
