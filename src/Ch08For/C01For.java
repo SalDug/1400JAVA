3f0a501f8ba0941f3ea1c92fbc58afe20ba73f26
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-07-18 16:25:33 +0900

    for

--- src/Ch08For/C01For.java
+++ src/Ch08For/C01For.java
@@ -1,10 +1,29 @@
 package Ch08For;
 
+import java.util.Scanner;
+
 public class C01For {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//n, m
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int sum = 0;
+		for(int i = n;i<=m;i++) {
+			if(i%3 !=0) {
+				sum += i;
+			}
+		}
+		System.out.printf("%d\n", sum);
+		//구구단
+		for(int i = 2;i<=9;i++) {
+			for(int j= 1;j<9;j++) {
+				System.out.printf("%d x %d = %d\n", i, j, i*j);
+			}
+		}
+		
+		sc.close();
 	}
 
 }
