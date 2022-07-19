30e95fd695dcacf3690f5bd5a6257b61f5ecd957
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-07-19 16:47:57 +0900

    method

--- src/Ch09ClassBasic/C06MethodTest.java
+++ src/Ch09ClassBasic/C06MethodTest.java
@@ -3,7 +3,7 @@ package Ch09ClassBasic;
 import java.util.Scanner;
 
 class SumCal{
-	
+	Scanner sc = new Scanner(System.in);
 	//인자 o 반환 o
 	int sum1(int x, int y) {
 		return x+y;
@@ -12,19 +12,33 @@ class SumCal{
 	void sum2(int x, int y) {
 		System.out.println("void sum2(x, y) = " + (x+y));
 	}
+	//인자 x 반환 o
+	int sum3() {
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		return (x + y);
+	}
+	//인자 x 반환 x
+	void sum4() {
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		System.out.println("void sum4(x, y) = " + (x+y));
+	}
+	
 }
 
 public class C06MethodTest {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		//객체 생성
 		SumCal obj1 = new SumCal();
 		
 		int result = obj1.sum1(10, 20);
 		System.out.println(result);
 		obj1.sum2(30, 40);
-		sc.close();
+		int result3 = obj1.sum3();
+		System.out.println(result3);
+		obj1.sum4();
 	}
 
 }
