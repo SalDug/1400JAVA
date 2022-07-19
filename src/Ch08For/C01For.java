63dddce5840ae470513b6b63b28f9397b0362917
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-07-19 14:18:38 +0900

    for

--- src/Ch08For/C01For.java
+++ src/Ch08For/C01For.java
@@ -6,24 +6,40 @@ public class C01For {
 
 	public static void main(String[] args) {
 		//n, m
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int sum = 0;
-		for(int i = n;i<=m;i++) {
-			if(i%3 !=0) {
-				sum += i;
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		int m = sc.nextInt();
+//		int sum = 0;
+//		for(int i = n;i<=m;i++) {
+//			if(i%3 !=0) {
+//				sum += i;
+//			}
+//		}
+//		System.out.printf("%d\n", sum);
+//		//구구단
+//		for(int i = 2;i<=9;i++) {
+//			for(int j= 1;j<9;j++) {
+//				System.out.printf("%d x %d = %d\n", i, j, i*j);
+//			}
+//		}
+//		
+//		sc.close();
+		for (int i = 1;i<=7;i++) {
+			if(i == 1 || i == 7) {
+				System.out.println("   *   ");
 			}
-		}
-		System.out.printf("%d\n", sum);
-		//구구단
-		for(int i = 2;i<=9;i++) {
-			for(int j= 1;j<9;j++) {
-				System.out.printf("%d x %d = %d\n", i, j, i*j);
+			if(i == 2 || i == 6) {
+				System.out.println("  ***  ");
+			}
+			if(i == 3 || i == 5) {
+				System.out.println(" ***** ");
+			}
+			if(i == 4) {
+				System.out.println("*******");
 			}
 		}
 		
-		sc.close();
+		
 	}
 
 }
