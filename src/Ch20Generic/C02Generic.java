d19056e359ae9dcbf3b259480ba00b5c0cc07051
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-08-01 16:04:10 +0900

    generic

--- src/Ch20Generic/C02Generic.java
+++ src/Ch20Generic/C02Generic.java
@@ -1,9 +1,21 @@
 package Ch20Generic;
 
+class simple<T,S>{
+	T x;
+	S y;
+}
+
+
 public class C02Generic {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		simple<String, String> ob1 = new simple<String, String>();
+		ob1.x = "문자열1";
+		ob1.y = "문자열2";
+		
+		simple<Integer, Double> ob2 = new simple<Integer, Double>();
+		ob2.x = 10;
+		ob2.y = 10.5;
 
 	}
 
